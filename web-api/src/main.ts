fb1dc6bf7fe928217ac623fb16bbe6d6d101fa18
Nicolas Kolumbic <nicolaskolumbic@hotmail.com>
2023-11-01 23:59:45 -0300

    provide for the API to receive data that is not found in the data models

--- web-api/src/main.ts
+++ web-api/src/main.ts
@@ -1,10 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // configurar un prefijo globalmente para todos los endpoints.
   app.setGlobalPrefix('api/v1');
+
+  // prever que la API reciba datos que no se encuentran en los modelos de datos.
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    forbidNonWhitelisted: true
+  }))
+  
   await app.listen(3000);
 }
 bootstrap();

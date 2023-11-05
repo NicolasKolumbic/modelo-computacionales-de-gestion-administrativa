ba9937e203cd1080b20707e7d94f50b9067f9355
Nicolas Kolumbic <nicolaskolumbic@hotmail.com>
2023-11-05 19:20:23 -0300

    add odm prisma for map entities

--- web-api/src/product/entities/product.entity.ts
+++ web-api/src/product/entities/product.entity.ts
@@ -1,2 +1,8 @@
 // Entidades relacionadas con la BASE DE DATOS, cada una representa una COLLECION de la Base MongoDB 
-export class Product {}
+export class Product {
+    id: string;       
+    description: string;
+    price:     number;
+    stock: number;
+    category: {id: string; description: string}
+}

0f5c3a73e371daa1df58915743ea37637b9562ae
Nicolas Kolumbic <nicolaskolumbic@hotmail.com>
2023-11-05 21:25:49 -0300

    add categories resource to web api

--- web-api/src/app.module.ts
+++ web-api/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ProductModule } from './product/product.module';
+import { CategoryModule } from './category/category.module';
 
 @Module({
   imports: [
-    ProductModule
-  ]
+    ProductModule,
+    CategoryModule
+  ],
+  providers: []
 })
 export class AppModule {}
